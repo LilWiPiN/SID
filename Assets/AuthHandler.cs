ddc0d7fd2a94e9880a87f62008bb14d654c82343
Valentin Arias <valentin.arias@upb.edu.co>
2024-09-02 15:58:56 -0500

    Update Actividad 2

--- Assets/AuthHandler.cs
+++ Assets/AuthHandler.cs
@@ -1,13 +1,15 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using TMPro;
+using Unity.VisualScripting;
 using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEditor.PackageManager.Requests;
 using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class AuthHandler : MonoBehaviour
@@ -16,19 +18,165 @@ public class AuthHandler : MonoBehaviour
     [SerializeField] string _token;
     [SerializeField] string _username;
 
+    public GameObject mainMenu;
+    public GameObject menuScreen;
+    public GameObject signupScreen;
+    public GameObject loginScreen;
+    public GameObject ProfileScreen;
+    public GameObject LeaderboardScreen;
+
+    public GameObject backButton;
+
+    public GameObject inputErrors;
+    public GameObject inputError1;
+    public GameObject inputError2;
+    public GameObject inputError3;
+
+    public GameObject inputError4;
+
+    public GameObject inputCorrect1;
+    public GameObject inputCorrect2;
+
+    public GameObject invalidToken;
+    public GameObject username;
+    public RawImage imgProfile;
+    public Texture[] rndTexture;
+
+    public GameObject inputScore;
+    public GameObject inputColor;
+    public GameObject inputLevel;
+    public GameObject inputExp;
+
+    public GameObject inputUpdate;
+
+    public GameObject leaderboardUsername;
+    public GameObject leaderboardScore;
+    public GameObject leaderboardLevel;
+    public GameObject leaderboardExp;
+    string tagUsername, tagScore, tagLevel, tagExp;
+    int index;
+
     void Start()
     {
+        index = 0;
+
+        SetOff();
+
         _token = PlayerPrefs.GetString("token");
         _username = PlayerPrefs.GetString("username");
 
         if (string.IsNullOrEmpty(_token) || string.IsNullOrEmpty(_username))
-        {
             Debug.Log("No hay token");
-        }
         else
-        {
             StartCoroutine(GetProfile());
-        }
+    }
+
+    public void SetOff()
+    {
+        mainMenu = GameObject.Find("Main");
+        menuScreen = GameObject.Find("Menu");
+        signupScreen = GameObject.Find("SignUpScreen");
+        loginScreen = GameObject.Find("LogInScreen");
+        ProfileScreen = GameObject.Find("Profile");
+        LeaderboardScreen = GameObject.Find("Leaderboard");
+
+        backButton = GameObject.Find("Back");
+
+        inputErrors = GameObject.Find("Errors");
+        inputError1 = GameObject.Find("ExistingUser");
+        inputError2 = GameObject.Find("EnterPassword");
+        inputError3 = GameObject.Find("EnterUser");
+
+        inputError4 = GameObject.Find("NoExistingUser");
+
+        inputCorrect1 = GameObject.Find("AutheticatedUser"); 
+        inputCorrect2 = GameObject.Find("CreatedUser");
+
+        username = GameObject.Find ("User");
+        invalidToken = GameObject.Find("InvalidToken");
+
+        inputScore = GameObject.Find("InputScore");
+        inputColor = GameObject.Find("InputColor");
+        inputLevel = GameObject.Find("InputLevel");
+        inputExp = GameObject.Find("InputExp");
+
+        inputUpdate = GameObject.Find("UpdatedInfo");
+
+        leaderboardUsername = GameObject.Find($"UserText{index}");
+        leaderboardScore = GameObject.Find($"UserScore{index}");
+        leaderboardLevel = GameObject.Find($"UserLevel{index}");
+        leaderboardExp = GameObject.Find($"UserExp{index}");
+
+        menuScreen.SetActive(false);
+        ProfileScreen.SetActive(false);
+        LeaderboardScreen.SetActive(false);
+    }
+
+    public void BackMain()
+    {
+        mainMenu.SetActive(true);
+        menuScreen.SetActive(false);
+        ProfileScreen.SetActive(false);
+        LeaderboardScreen.SetActive(false);
+    }
+
+    public void ExitProfile()
+    {
+        mainMenu.SetActive(true);
+        ProfileScreen.SetActive(false);
+        LeaderboardScreen.SetActive(false);
+
+        TextMeshProUGUI tmpText = invalidToken.GetComponent<TextMeshProUGUI>();
+        tmpText.text = "La sesion de " + _username + " ha sido suspendida";
+        invalidToken.SetActive(true);
+    }
+
+    public void Res()
+    {
+        inputError1.SetActive(false);
+        inputError2.SetActive(false);
+        inputError3.SetActive(false);
+        inputError4.SetActive(false);
+
+        inputCorrect1.SetActive(false);
+        inputCorrect2.SetActive(false);
+
+        invalidToken.SetActive(false);
+        username.SetActive(false);
+
+        inputUpdate.SetActive(false);
+    }
+
+    public void SignUpScreen() 
+    {
+        mainMenu.SetActive(false);
+        menuScreen.SetActive(true);
+        signupScreen.SetActive(true);
+        loginScreen.SetActive(false);
+
+        Res();
+    }
+    
+    public void LogInScreen()
+    {
+        mainMenu.SetActive(false);
+        menuScreen.SetActive(true);
+        loginScreen.SetActive(true);
+        signupScreen.SetActive(false);
+
+        Res();
+    }
+
+    public void SignUp()
+    {
+        JsonData jsonData = new JsonData();
+
+        jsonData.username = GameObject.Find("InputUsername").GetComponent<TMP_InputField>().text;
+        jsonData.password = GameObject.Find("InputPassword").GetComponent<TMP_InputField>().text;
+
+        string postData = JsonUtility.ToJson(jsonData);
+
+        StartCoroutine(SignUpPost(postData));
     }
 
     public void LogIn()
@@ -42,16 +190,21 @@ public class AuthHandler : MonoBehaviour
 
         StartCoroutine(LogInPost(postData));
     }
-    public void SignUp()
+
+    public void UpdateData()
     {
-        JsonData jsonData = new JsonData();
+        UserData userData = new UserData();
 
-        jsonData.username = GameObject.Find("InputUsername").GetComponent<TMP_InputField>().text;
-        jsonData.password = GameObject.Find("InputPassword").GetComponent<TMP_InputField>().text;
+        TMP_Dropdown dropdown = GameObject.Find("InputColor").GetComponent<TMP_Dropdown>();
 
-        string postData = JsonUtility.ToJson(jsonData);
+        userData.score = int.Parse(GameObject.Find("InputScore").GetComponent<TMP_InputField>().text);
+        userData.color = dropdown.options[dropdown.value].text;
+        userData.level = (int)GameObject.Find("InputLevel").GetComponent<Slider>().value;
+        userData.exp = (int)GameObject.Find("InputExp").GetComponent<Slider>().value;
+        
+        string postData = JsonUtility.ToJson(userData);
 
-        StartCoroutine(SignUpPost(postData));
+        StartCoroutine(PatchUsers(postData));
     }
 
     IEnumerator SignUpPost(string postData)
@@ -66,14 +219,37 @@ public class AuthHandler : MonoBehaviour
             if (request.responseCode == 200)
             {
                 Debug.Log(request.downloadHandler.text);
+                inputCorrect1.SetActive(false);
+                inputCorrect2.SetActive(true);
                 StartCoroutine(LogInPost(postData));
             }
             else
             {
+                inputErrors.SetActive(true);
+                inputCorrect2.SetActive(false);
                 string msj = "status: " + request.responseCode;
                 msj += "\nError: " + request.error;
                 msj += "\n" + request.downloadHandler.text;
                 Debug.Log(msj);
+
+                switch (request.downloadHandler.text)
+                {
+                    case "{\"msg\":\"Debe enviar el usuario\"}":
+                        inputError1.SetActive(false);
+                        inputError2.SetActive(false);
+                        inputError3.SetActive(true);
+                        break;
+                    case "{\"msg\":\"Debe enviar el password\"}":
+                        inputError1.SetActive(false);
+                        inputError2.SetActive(true);
+                        inputError3.SetActive(false);
+                        break;
+                    case "{\"msg\":\"Ya existe usuario con ese username\"}":
+                        inputError1.SetActive(true);
+                        inputError2.SetActive(false);
+                        inputError3.SetActive(false);
+                        break;
+                }
             }
         }
     }
@@ -98,18 +274,48 @@ public class AuthHandler : MonoBehaviour
                 _token = authData.token;
                 _username = authData.usuario.username;
 
-                GameObject.Find("Canvas").SetActive(false);
+                inputCorrect2.SetActive(false);
+                inputCorrect1.SetActive(true);
+                inputErrors.SetActive(false);
+
+                StartCoroutine(GetProfile());
             }
             else
             {
+                inputErrors.SetActive(true);
+                inputCorrect1.SetActive(false);
                 string msj = "status: " + request.responseCode;
                 msj += "\nError: " + request.error;
                 msj += "\n" + request.downloadHandler.text;
                 Debug.Log(msj);
+
+                switch (request.downloadHandler.text)
+                {
+                    case "{\"msg\":\"debe enviar el campo username en la petición\",\"field\":\"username\"}":
+                        inputError2.SetActive(false);
+                        inputError3.SetActive(true);
+                        inputError4.SetActive(false);
+                        break;
+                    case "{\"msg\":\"debe enviar el campo password en la petición\",\"field\":\"password\"}":
+                        inputError2.SetActive(true);
+                        inputError3.SetActive(false);
+                        inputError4.SetActive(false);
+                        break;
+                    case "{\"msg\":\"Usuario o contraseña no son correctos - password\"}":
+                        inputError2.SetActive(false);
+                        inputError3.SetActive(false);
+                        inputError4.SetActive(true);
+                        break;
+                    case "{\"msg\":\"Usuario o contraseña no son correctos - correo\"}":
+                        inputError2.SetActive(false);
+                        inputError3.SetActive(false);
+                        inputError4.SetActive(true);
+                        break;
+                }
             }
         }
     }
-
+    
     IEnumerator GetProfile()
     {
         string url = URL + "api/usuarios/" + _username;
@@ -128,19 +334,34 @@ public class AuthHandler : MonoBehaviour
                 Debug.Log(request.downloadHandler.text);
                 Debug.Log(user.username + " esta en nivel " + user.data.level + " y tiene puntaje " + user.data.score);
 
-                GameObject.Find("Canvas").SetActive(false);
+                ProfileScreen.SetActive(true);
+                username.SetActive(true);
+
+                mainMenu.SetActive(false);
+                menuScreen.SetActive(false);
+                loginScreen.SetActive(false);
+
+                TextMeshProUGUI tmpText = username.GetComponent<TextMeshProUGUI>();
+                tmpText.text = _username;
+                
+                int rnd = Random.Range(0, 10);
+                imgProfile.texture = rndTexture[rnd];
             }
             else
             {
-                Debug.Log("El token del usuario " + _username + " ya no es valido");
-                GameObject.Find("Canvas").SetActive(true);
+                Debug.Log("El token del usuario " + _username + " ya no es valido - 401");
+                TextMeshProUGUI tmpText = invalidToken.GetComponent<TextMeshProUGUI>();
+                tmpText.text = "La sesion de " + _username + " ha sido suspendida";
+
+                mainMenu.SetActive(true);
+                invalidToken.SetActive(true);
             }
         }
     }
 
     IEnumerator GetUsers()
     {
-        string url = URL + "/api/usuarios";
+        string url = URL + "api/usuarios";
         UnityWebRequest request = UnityWebRequest.Get(url);
         request.SetRequestHeader("x-token", _token);
 
@@ -151,11 +372,34 @@ public class AuthHandler : MonoBehaviour
         {
             if (request.responseCode == 200)
             {
+                AuthData data = JsonUtility.FromJson<AuthData>(request.downloadHandler.text);
+                //User userK = data.usuario;
+                //Debug.Log(userK.username + " esta en nivel " + userK.data.level + " y tiene puntaje " + userK.data.score);
+
+                Debug.Log(request.downloadHandler.text);
+
+                TextMeshProUGUI usernameText = leaderboardUsername.GetComponent<TextMeshProUGUI>();
+                TextMeshProUGUI scoreText = leaderboardScore.GetComponent<TextMeshProUGUI>();
+                TextMeshProUGUI levelText = leaderboardLevel.GetComponent<TextMeshProUGUI>();
+                TextMeshProUGUI expText = leaderboardExp.GetComponent<TextMeshProUGUI>();
+
+                int rnd = Random.Range(0, 10);
+
+                LeaderboardScreen.SetActive(true);
+                ProfileScreen.SetActive(false);
+
                 UserList users = JsonUtility.FromJson<UserList>(request.downloadHandler.text);
 
                 foreach (var user in users.usuarios)
                 {
+                    imgProfile.texture = rndTexture[rnd];
 
+                    tagUsername = usernameText.text = $"<color={user.data.color.ToLower()}>{user.username}</color>";
+                    tagScore = scoreText.text = user.data.score.ToString();
+                    tagLevel = levelText.text = user.data.level.ToString();
+                    tagExp = expText.text = user.data.exp.ToString();
+
+                    index++;
                 }
 
                 List<User> leaderboard = users.usuarios.OrderByDescending(u => u.data.score).ToList();
@@ -170,21 +414,29 @@ public class AuthHandler : MonoBehaviour
         }
     }
 
-    IEnumerator PatchUsers(int score)
+    IEnumerator PatchUsers(string postData)
     {
         string url = URL + "api/usuarios";
-        UnityWebRequest request = UnityWebRequest.Put(url, "body");
+        string body = "{\"username\":\"" + _username + "\",\"data\":" + postData + "}";
+        
+        UnityWebRequest request = UnityWebRequest.Put(url, body);
         request.method = "PATCH";
+        request.SetRequestHeader("Content-Type", "application/json");
         request.SetRequestHeader("x-token", _token);
 
         yield return request.SendWebRequest();
+
         if (request.result == UnityWebRequest.Result.ConnectionError)
             Debug.Log(request.error);
         else
         {
             if (request.responseCode == 200)
             {
-                //ESCRIBIR
+                Debug.Log(request.downloadHandler.text);
+
+                inputUpdate.SetActive(true);
+
+                StartCoroutine(GetUsers());
             }
             else
             {
@@ -193,6 +445,7 @@ public class AuthHandler : MonoBehaviour
                 msj += "\n" + request.downloadHandler.text;
                 Debug.Log(msj);
             }
+
         }
     }
 
@@ -227,8 +480,8 @@ public class AuthHandler : MonoBehaviour
     public class UserData
     {
         public int score;
+        public string color;
         public int level;
         public int exp;
-        public string color;
     }
 }
